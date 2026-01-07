19139a8d598fb57f5ac9e1382d56ac0a82ea67d4
UranixWeb <horatom22v@gyri.cz>
2026-01-07 15:58:23 +0100

    table update

--- navbar-script.js
+++ navbar-script.js
@@ -1,61 +1,13 @@
 const openButton = document.getElementById('open-sidebar-button')
 const navbar = document.getElementById('navbar')
-const signUp = document.getElementById('signUp')
 const overlay = document.getElementById('overlay')
-const username = document.getElementById('username')
-const password = document.getElementById('password')
-const signUsername = document.getElementById('signUsername')
-const signPassword = document.getElementById('signPassword')
-
-const media = window.matchMedia("(width < 700px)")
-
+const media = window.matchMedia("(width < 1000px)")
 media.addEventListener('change', (e) => updateNavbar(e))
-
-function updateNavbar(e){
-  const isMobile = e.matches
-  console.log(isMobile)
-  if(isMobile){
-    navbar.setAttribute('inert', '')
-  }
-  else{
-    // desktop device
-    navbar.removeAttribute('inert')
-  }
-}
 
 function openSidebar(){
   navbar.classList.add('show')
-  openButton.setAttribute('aria-expanded', 'true')
-  navbar.removeAttribute('inert')
 }
 
 function closeSidebar(){
   navbar.classList.remove('show')
-  openButton.setAttribute('aria-expanded', 'false')
-  navbar.setAttribute('inert', '')
 }
-
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
