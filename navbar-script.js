75be83a455231dcbfb11b9d9e36f618fbbdf6de9
UranixWeb <horatom22v@gyri.cz>
2026-02-01 16:17:03 +0100

    t

--- navbar-script.js
+++ navbar-script.js
@@ -1,8 +1,4 @@
-//const openButton = document.getElementById('open-sidebar-button')
 const navbar = document.getElementById('navbar')
-//const overlay = document.getElementById('overlay')
-//const media = window.matchMedia("(width < 1000px)")
-//media.addEventListener('change', (e) => updateNavbar(e))
 
 function openSidebar(){
   navbar.classList.add('show')
