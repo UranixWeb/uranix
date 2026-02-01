1cb2621da9c4f5ada47d837ad1cba0fdadc9bac3
UranixWeb <horatom22v@gyri.cz>
2026-02-01 15:16:28 +0100

    test

--- navbar-script.js
+++ navbar-script.js
@@ -1,8 +1,8 @@
-const openButton = document.getElementById('open-sidebar-button')
+//const openButton = document.getElementById('open-sidebar-button')
 const navbar = document.getElementById('navbar')
-const overlay = document.getElementById('overlay')
-const media = window.matchMedia("(width < 1000px)")
-media.addEventListener('change', (e) => updateNavbar(e))
+//const overlay = document.getElementById('overlay')
+//const media = window.matchMedia("(width < 1000px)")
+//media.addEventListener('change', (e) => updateNavbar(e))
 
 function openSidebar(){
   navbar.classList.add('show')
